35d013d013d540eccb9e25b16660392b5d36abd9
gbanm <gbanm06@gmail.com>
2020-01-05 23:47:00 +0100

    Added integrate

--- Numerics/Numerics/NumericExtensions.cs
+++ Numerics/Numerics/NumericExtensions.cs
@@ -4,8 +4,19 @@ namespace Numerics
 {
     public static class NumericExtensions
     {
+        public const double h = 0.0000001;
 
-        public static double Derivate(this Func<double, double> func, double variablevalue) => (func(variablevalue + 0.0000001) - func(variablevalue)) / 0.0000001;
+        public static double Derivate(this Func<double, double> func, double variablevalue) => (func(variablevalue + h) - func(variablevalue)) /h;
+
+        public static double Integrate(this Func<double, double> func, double lowerlimit, double upperlimit)
+        {
+            var result = 0.0;
+            for (var i = lowerlimit; i <= upperlimit; i+=h)
+            {
+                result += h*(func(i) + func(i + h)) / 2;
+            }
+            return result;
+        }
 
     }
 }
--- Numerics/NumericsTests/NumericsTests.cs
+++ Numerics/NumericsTests/NumericsTests.cs
@@ -16,7 +16,16 @@ namespace NumericsTests
             Func<double, double> func = (double variable) => g * Math.Pow(variable, 2) / 2;
             var t = 5;
             var result = func.Derivate(t);
-            Assert.IsTrue(Math.Round(result,2) == Math.Round(g * t, 2));
+            Assert.IsTrue(Math.Round(result) == Math.Round(g * t));
+        }
+        [TestMethod]
+        public void TestIntegrateExponentiation()
+        {
+            Func<double, double> func = (double variable) => g * variable;
+            var lowerlimit = 0;
+            var upperlimit = 5;
+            var result = func.Integrate(lowerlimit, upperlimit);
+            Assert.IsTrue(Math.Round(result) == Math.Round(g * Math.Pow(upperlimit, 2) / 2));
         }
         [TestMethod]
         public void TestDerivateExponential()
@@ -28,6 +37,16 @@ namespace NumericsTests
         }
 
         [TestMethod]
+        public void TestIntegrateExponential()
+        {
+            Func<double, double> func = (double variable) => Math.Pow(Math.E, variable);
+            var lowerlimit = 0;
+            var upperlimit = 5;
+            var result = func.Integrate(lowerlimit, upperlimit);
+            Assert.IsTrue(Math.Round(result,2) == Math.Round(Math.Pow(Math.E, upperlimit) - Math.Pow(Math.E, lowerlimit),2));
+        }
+
+        [TestMethod]
         public void TestDerivateSinus()
         {
             Func<double, double> func = (double variable) => Math.Sin(variable);
@@ -35,5 +54,17 @@ namespace NumericsTests
             var result = func.Derivate(t);
             Assert.IsTrue(Math.Round(result) == Math.Round(Math.Cos(t)));
         }
+
+        [TestMethod]
+        public void TestIntegrateCosinus()
+        {
+            Func<double, double> func = (double variable) => Math.Round(Math.Cos(variable));
+            var lowerlimit = 0;
+            var upperlimit = Math.PI/5;
+            var result = func.Integrate(lowerlimit, upperlimit);
+            Assert.IsTrue(Math.Round(result) == Math.Round(Math.Sin(upperlimit)));
+        }
+
+
     }
 }
