207c143a807a3b142d2bc89d5fc574263fd7fb1b
gbanm <gbanm06@gmail.com>
2023-01-29 15:19:08 +0100

    Pass testing

--- Numerics/Numerics/Objects/NeuralNetwork.cs
+++ Numerics/Numerics/Objects/NeuralNetwork.cs
@@ -19,90 +19,16 @@ namespace Numerics.Objects
             this.fitness = fitness;
             this.activateFunction =activateFunction;
             this.layers = new int[layers.Length];
-            for (var i = 0; i < layers.Length; i++)
-            {
-                this.layers[i] = layers[i];
-            }
+       
+            this.activateFunction = activateFunction;
+            this.layers = new int[layers.Length];
+            neurons = new Tensor(Array.Empty<double>());
+            biases = new Tensor(Array.Empty<double>());
 
-            var neuronList = new List<double[]>();
-
-            for (var i = 0; i < layers.Length; i++)
-            {
-                neuronList.Add(new double[layers[i]]);
-            }
-            neurons = new Tensor(neuronList.ToArray());
-            var biasesList = new List<double[]>();
-            for (var i = 0; i < layers.Length; i++)
-            {
-                var bias = new double[layers[i]];
-                for (var j = 0; j < layers[i]; j++)
-                {
-                   
-                    bias[j] = new Random().RandomDouble(-0.5f, 0.5f); 
-                }
-                biasesList.Add(bias);
-            }
-
-           biases = new Tensor(biasesList.ToArray());
-           var weightsList = new List<double[][]>();
-
-            for (var i = 1; i < layers.Length; i++)
-            {
-                var layerWeightsList = new List<double[]>();
-                var  neuronsInPreviousLayer = layers[i - 1];
-
-                for (var j = 0; j < neurons.values.GetLength(i); j++)
-                {
-                    var neuronWeights = new double[neuronsInPreviousLayer];
-                    for (var k = 0; k < neuronsInPreviousLayer; k++)
-                    {
-                        
-                        neuronWeights[k] = new Random().RandomDouble(-0.5f, 0.5f);
-                    }
-                    layerWeightsList.Add(neuronWeights);
-                }
-                weightsList.Add(layerWeightsList.ToArray());
-            }
-            weights = new Tensor(weightsList.ToArray());
+            weights = new Tensor(Array.Empty<double>());
 
         }
-        public int CompareTo(NeuralNetwork other)
-        {
-
-            if (fitness > other.fitness)
-            {
-                return 1;
-            }
-                  
-            if (fitness < other.fitness)
-            {
-                return -1;
-            }
-                 
-                return 0;
-        }
-        public double[] FeedForward(double[] inputs)
-        {
-            for (var i = 0; i < inputs.Length; i++)
-            {
-                neurons.values.SetValue(inputs[i],0, i);
-            }
-            for (var i = 1; i < layers.Length; i++)
-            {
-
-                for (var j = 0; j < neurons.values.GetLength(i); j++)
-                {
-                    var value = 0.0;
-                    for (var k = 0; k < neurons.values.GetLength(i - 1); k++)
-                    {
-                        value += Convert.ToDouble(weights.values.GetValue(i - 1, j, k)) * Convert.ToDouble(neurons.values.GetValue(i - 1, k));
-                    }
-                    neurons.values.SetValue(activateFunction(value + Convert.ToDouble(biases.values.GetValue(i,j))),i,j) ;
-                }
-            }
-            return (double[])neurons.values.GetValue(neurons.values.Length - 1);
-        }
-
+      
 
         public double[] Train(double[] features, double[] label, int epoch)
         {
