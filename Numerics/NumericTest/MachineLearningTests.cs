65abd8c355a36b874f03950c89c5453e11a915f8
gbanm <gbanm06@gmail.com>
2023-04-02 20:50:57 +0200

    saved

--- Numerics/NumericTest/MachineLearningTests.cs
+++ Numerics/NumericTest/MachineLearningTests.cs
@@ -13,6 +13,7 @@ namespace NumericTest
             var labels = new double[] { 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 };
             int[] layers = new int[] { 10, 10 };
             Func<double, double> activate = (x) => 1 / (1 + Math.Exp(-x));
+            Func<double, double> loss = (x) => ;
 
             var neuralNetwork = new NeuralNetwork(layers, 0.0007, activate);
             neuralNetwork.Train(features, labels, 100);
--- Numerics/Numerics/Objects/NeuralNetwork.cs
+++ Numerics/Numerics/Objects/NeuralNetwork.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 
 public  class NeuralNetwork
 {
@@ -19,26 +18,25 @@ public  class NeuralNetwork
 
     public void Train(double[] features, double[] labels, int epochs)
     {
-        int n = features.Length;
+        var n = features.Length;
 
-        // Initialize model parameters
+       
         weight = 0.0;
         bias = 0.0;
 
-        // Perform gradient descent
+     
         for (int i = 0; i < epochs; i++)
         {
-            // Compute predictions
-            double[] predictions = new double[n];
+           
+            var predictions = new double[n];
             for (int j = 0; j < n; j++)
             {
                 predictions[j] = Predict(features[j]);
             }
 
-            // Compute gradients
-            double dw = 0.0;
-            double db = 0.0;
-            for (int j = 0; j < n; j++)
+            var dw = 0.0;
+            var db = 0.0;
+            for (var j = 0; j < n; j++)
             {
                 dw += (predictions[j] - labels[j]) * features[j];
                 db += (predictions[j] - labels[j]);
@@ -46,15 +44,15 @@ public  class NeuralNetwork
             dw /= n;
             db /= n;
 
-            // Update model parameters
             weight -= learningRate * dw;
             bias -= learningRate * db;
         }
     }
 
+
     public double Predict(double input)
     {
-        double output = input * weight + bias;
+        var output = input * weight + bias;
         return output;
     }
 }
