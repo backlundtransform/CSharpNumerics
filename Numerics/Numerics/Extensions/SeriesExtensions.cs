e29d1115eee345765393541f69a22f61e38dfbb7
gbanm <gbanm06@gmail.com>
2024-04-01 22:26:02 +0200

    re added GenerateTimeSerieWithEquivalentSteps

--- Numerics/Numerics/Extensions/SeriesExtensions.cs
+++ Numerics/Numerics/Extensions/SeriesExtensions.cs
@@ -73,6 +73,61 @@ namespace System
       
         }
 
+        public static List<TimeSerie> GenerateTimeSerieWithEquivalentSteps(this List<TimeSerie> timeSeries,
+           int minutes,
+           DateTime startDate,
+           DateTime endDate,
+           GroupOperator groupOperator = GroupOperator.Average,
+           int multiplier = 1,
+           bool shouldInterpolate = true)
+        {
+            var groups = timeSeries.GroupBy(x =>
+            {
+                var stamp = x.TimeStamp;
+                stamp = stamp.AddMinutes(-(stamp.Minute % minutes));
+                stamp = stamp.AddMilliseconds(-stamp.Millisecond - 1000 * stamp.Second);
+                return stamp;
+            }).ToDictionary(g => g.Key, g => g.ToList());
+
+            var newTimeserie = new List<TimeSerie>();
+
+            for (var date = startDate; date <= endDate; date = date.AddMinutes(minutes))
+            {
+                var value = 0.0;
+
+                if (!groups.ContainsKey(date) && shouldInterpolate)
+                {
+                    value = timeSeries.LinearInterpolationTimeSerie(date);
+                }
+
+                if (groups.ContainsKey(date))
+                {
+                    switch (groupOperator)
+                    {
+                        case GroupOperator.Average:
+                            value = groups[date].Average(p => p.Value);
+                            break;
+                        case GroupOperator.Max:
+                            value = groups[date].Max(p => p.Value);
+                            break;
+                        case GroupOperator.Min:
+                            value = groups[date].Min(p => p.Value);
+                            break;
+                        case GroupOperator.Sum:
+                            value = groups[date].Sum(p => p.Value);
+                            break;
+                        case GroupOperator.Median:
+                            value = groups[date].Median(p => p.Value);
+                            break;
+                    }
+                }
+
+                newTimeserie.Add(new TimeSerie { TimeStamp = date, Value = (multiplier) * value });
+
+            }
+            return newTimeserie;
+        }
+
         public static double Interpolation(this long currentTicks,
             long firstTicks, 
             long lastTicks, 
