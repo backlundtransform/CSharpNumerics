b03acb47a9b3df97c71594910c5bddbaa807ee8f
backlundtransform <gbanm06@gmail.com>
2025-11-30 13:12:27 +0100

    dtage all

--- Numerics/NumericTest/MachineLearningTests.cs
+++ Numerics/NumericTest/MachineLearningTests.cs
@@ -6,27 +6,7 @@ namespace NumericTest
     [TestClass]
     public class MachineLearningTests
     {
-        [TestMethod]
-        public void MachineLearning()
-        {
-            var features = new Tensor(new double[,] { { 1, 2 }, { 3, 4 } , { 5, 6 } });
-            var labels = new Tensor(new double[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
-            int[] layers = new int[] { 4, 4, 4 };
-            Func<double, double> activate = (x) => Math.Tanh(x);
-          
-
-
-            var neuralNetwork = new NeuralNetwork(layers, 0.1, activate);
-            neuralNetwork.Train(features, labels, 1);
-         
-
-            var input = new Tensor(new double[,] { { 1, 2 }, { 3, 4 }, { 5, 6 } });
-            var result = neuralNetwork.Predict(input);
-
-
-            Assert.AreEqual(7, result[0],0.2);
-
-        }
+        
 
       
     }
--- Numerics/Numerics/Regression/Linear.cs
+++ Numerics/Numerics/Regression/Linear.cs
@@ -6,95 +6,94 @@ using System.Threading.Tasks;
 
 namespace CSharpNumerics.Regression
 {
-    namespace CSharpNumerics.Regression
+    internal class Linear
     {
-        internal class Linear
+
+        private double[] _weights;
+        private double _bias;
+
+
+        public double LearningRate { get; set; } = 0.01;
+        public int Epochs { get; set; } = 1000;
+        public double L2Regularization { get; set; } = 0.0;
+
+        public Linear(int featureCount)
         {
-            // Model parameters
-            private double[] _weights;  // w0 ... wn
-            private double _bias;       // b
+            _weights = new double[featureCount];
+            _bias = 0.0;
+        }
 
-            // Hyperparameters
-            public double LearningRate { get; set; } = 0.01;
-            public int Epochs { get; set; } = 1000;
-            public double L2Regularization { get; set; } = 0.0;  // lambda
+        public void Fit(double[][] X, double[] y)
+        {
+            int n = X.Length;
+            int d = _weights.Length;
 
-            public Linear(int featureCount)
+            for (int epoch = 0; epoch < Epochs; epoch++)
             {
-                _weights = new double[featureCount];
-                _bias = 0.0;
-            }
+                double[] gradW = new double[d];
+                double gradB = 0.0;
 
-            public void Fit(double[][] X, double[] y)
-            {
-                int n = X.Length;            // number of samples
-                int d = _weights.Length;     // number of features
 
-                for (int epoch = 0; epoch < Epochs; epoch++)
-                {
-                    double[] gradW = new double[d];
-                    double gradB = 0.0;
-
-                    // Compute gradients
-                    for (int i = 0; i < n; i++)
-                    {
-                        double pred = PredictSingle(X[i]);
-                        double error = pred - y[i];
-
-                        // accumulate gradients
-                        for (int j = 0; j < d; j++)
-                            gradW[j] += error * X[i][j];
-
-                        gradB += error;
-                    }
-
-                    // Apply L2 regularization on weights
-                    for (int j = 0; j < d; j++)
-                        gradW[j] = gradW[j] / n + L2Regularization * _weights[j];
-
-                    gradB /= n;
-
-                    // Update rule
-                    for (int j = 0; j < d; j++)
-                        _weights[j] -= LearningRate * gradW[j];
-
-                    _bias -= LearningRate * gradB;
-                }
-            }
-
-            public double Predict(double[] x)
-            {
-                return PredictSingle(x);
-            }
-
-            public double[] Predict(double[][] X)
-            {
-                var preds = new double[X.Length];
-                for (int i = 0; i < X.Length; i++)
-                    preds[i] = PredictSingle(X[i]);
-
-                return preds;
-            }
-
-            private double PredictSingle(double[] x)
-            {
-                double sum = _bias;
-                for (int j = 0; j < _weights.Length; j++)
-                    sum += _weights[j] * x[j];
-
-                return sum;
-            }
-
-            public double MeanSquaredError(double[][] X, double[] y)
-            {
-                double loss = 0;
-                int n = X.Length;
                 for (int i = 0; i < n; i++)
                 {
-                    double e = PredictSingle(X[i]) - y[i];
-                    loss += e * e;
+                    double pred = PredictSingle(X[i]);
+                    double error = pred - y[i];
+
+
+                    for (int j = 0; j < d; j++)
+                        gradW[j] += error * X[i][j];
+
+                    gradB += error;
                 }
-                return loss / n;
+
+
+                for (int j = 0; j < d; j++)
+                    gradW[j] = gradW[j] / n + L2Regularization * _weights[j];
+
+                gradB /= n;
+
+
+                for (int j = 0; j < d; j++)
+                    _weights[j] -= LearningRate * gradW[j];
+
+                _bias -= LearningRate * gradB;
             }
         }
+
+        public double Predict(double[] x)
+        {
+            return PredictSingle(x);
+        }
+
+        public double[] Predict(double[][] X)
+        {
+            var preds = new double[X.Length];
+            for (int i = 0; i < X.Length; i++)
+                preds[i] = PredictSingle(X[i]);
+
+            return preds;
+        }
+
+        private double PredictSingle(double[] x)
+        {
+            double sum = _bias;
+            for (int j = 0; j < _weights.Length; j++)
+                sum += _weights[j] * x[j];
+
+            return sum;
+        }
+
+        public double MeanSquaredError(double[][] X, double[] y)
+        {
+            double loss = 0;
+            int n = X.Length;
+            for (int i = 0; i < n; i++)
+            {
+                double e = PredictSingle(X[i]) - y[i];
+                loss += e * e;
+            }
+            return loss / n;
+        }
     }
+}
+ 
